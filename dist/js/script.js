816df105907fb7e0b42e31fffbbdaa663f78f312
DeRusOfficial <derus.ru@mail.ru>
2025-08-09 12:16:58 +0300

    Realize loading btn

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,91 @@
-const BEGIN_URL = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20';
+// const BEGIN_URL = `https://pokeapi.co/api/v2/pokemon/?offset=${beginNum}&limit=${endNum}`;
 
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
-let id = 1;
+
+// const IMG_URL = 'https://pokeapi.co/api/v2/pokemon-form/';
+
+
+let beginNum;
+let endNum;
+
+
+let id = 0;
+let biggerId = 20;
 
 let namesOfPokemon = [];
 
 
+// getPokemonsName();
+// getPokemonsInfo();
+
 
 async function getPokemonsName() {
-    let response = await fetch(BEGIN_URL);
+    beginNum = 0;
+    endNum = 20;
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${beginNum}&limit=${endNum}`);
     let responseToJson = await response.json();
-    console.log(responseToJson.results);
+
+    // console.log(responseImgJson.sprites);
     for(id; id < 20; id++) { 
-        namesOfPokemon.push(responseToJson.results[id].name)
-        console.log(responseToJson.results[id].name)
+        let responseImg = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${id + 1}`);
+        let responseImgJson = await responseImg.json();
+        console.log(responseToJson.results[id].name);
+        document.getElementById('wrapper').innerHTML += loadPokemons(id, responseToJson.results[id].name, responseImgJson.sprites.front_default);
     }
-    console.log(namesOfPokemon)
+    console.log(namesOfPokemon);
 }
 
-async function getPokemonsInfo() {
-    for(id; id < 10; id++) {
-        let response = await fetch(BASE_URL + `${id}`);
-        let responseToJson = await response.json();
-        console.log(responseToJson);
+// async function getPokemonsInfo() {
+//     for(id; id < 10; id++) {
+//         let response = await fetch(BASE_URL + `${id}`);
+//         let responseToJson = await response.json();
+//         console.log(responseToJson);
+//     }
+// }
+
+function loadPokemons(id, pokemonName, img) {
+    return `
+        <div class="wrapper__card">
+            <div class="wrapper__card_pokemon">
+                <div class="wrapper__card_pokemon-num">${id + 1}</div>
+                <div class="wrapper__card_pokemon-name">${pokemonName}</div>
+            </div>
+            <div class="wrapper__card_img">
+                <img src="${img}" alt="">
+            </div>
+            <div class="wrapper__card_pokemon">
+                <div class="wrapper__card_pokemon-feature">
+                    <img src="" alt="">
+                </div>
+                <div class="wrapper__card_pokemon-feature">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+
+
+async function loadMorePokemons() {
+    // console.log('Hello');
+    // console.log(id);
+    console.log(beginNum);
+    beginNum = beginNum + 20;
+    endNum = endNum + 20;
+    id = beginNum;
+    console.log(beginNum);
+    console.log(endNum);
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${beginNum}&limit=${endNum}`);
+    let responseToJson = await response.json();
+
+    // console.log(responseImgJson.sprites);
+    for(id; id < endNum; id++) { 
+        let responseImg = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${id + 1}`);
+        let responseImgJson = await responseImg.json();
+        console.log(responseToJson.results[id].name);
+        document.getElementById('wrapper').innerHTML += loadPokemons(id, responseToJson.results[id].name, responseImgJson.sprites.front_default);
     }
+    console.log(id);
+
 }
